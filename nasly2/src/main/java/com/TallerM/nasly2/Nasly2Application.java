ee591890f0b3dd486017ec1b8c974183e39f2f1d
devstiven12 <devstiven12@gmail.com>
2026-06-17 17:10:54 -0500

    se crea el proyecto

---
+++ nasly2/src/main/java/com/TallerM/nasly2/Nasly2Application.java
@@ -0,0 +1,13 @@
+package com.TallerM.nasly2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nasly2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nasly2Application.class, args);
+	}
+
+}
---
+++ nasly2/src/test/java/com/TallerM/nasly2/Nasly2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TallerM.nasly2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nasly2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
